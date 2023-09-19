d47f6b695feb2a29c3cdd8533964ff8c0fa7a8d2
chobolevel <rodaka123@naver.com>
2023-09-19 21:06:30 +0900

    Feat: add BoardController API Test

---
+++ src/test/java/com/example/jpa/BoardApiControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.jpa;
+
+import com.example.jpa.entity.Board;
+import com.example.jpa.repository.BoardRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BoardApiControllerTest {
+
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @Autowired
+  private BoardRepository boardRepository;
+
+  @Test
+  public void selectTest() {
+
+    Board findBoard = boardRepository.findOneById(1L);
+
+    String url = "http://localhost:" + port + "/api/board/1";
+
+    // given
+
+    // when
+    ResponseEntity<Board> responseEntity = restTemplate.getForEntity(url, Board.class);
+
+    // then
+    Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    Assertions.assertThat(responseEntity.getBody().getId()).isEqualTo(findBoard.getId());
+
+  }
+
+}
