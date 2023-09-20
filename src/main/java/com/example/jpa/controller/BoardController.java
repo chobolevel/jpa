c293b6771f6a987b5d9dd449ac6b1e31f07d799b
chobolevel <rodaka123@naver.com>
2023-09-20 20:27:54 +0900

    Refactor: add post mapping

--- src/main/java/com/example/jpa/controller/BoardController.java
+++ src/main/java/com/example/jpa/controller/BoardController.java
@@ -1,22 +1,28 @@
 package com.example.jpa.controller;
 
+import com.example.jpa.entity.Board;
 import com.example.jpa.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/board/")
 public class BoardController {
 
   private final BoardRepository boardRepository;
 
-  @GetMapping("/api/board/{id}")
+  @GetMapping("{id}")
   public ResponseEntity<?> boardList(@PathVariable Long id) {
     return new ResponseEntity<>(boardRepository.findOneById(id), HttpStatus.OK);
+  }
+
+  @PostMapping("")
+  public ResponseEntity<?> create(@RequestBody Board board) {
+    boardRepository.save(board);
+    return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
 }
